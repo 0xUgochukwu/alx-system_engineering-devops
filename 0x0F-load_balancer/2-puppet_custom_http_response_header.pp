4f4476f86d02c9611e1e54601a539046b31e602d
0xUgochukwu <0xUgochukwu@gmail.com>
2023-09-05 14:23:45 -0700

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,35 +1,8 @@
-# set up custom header response
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
-}
-
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-get-update'],
-}
-
-file_line { 'Set up Redirection':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me http://100.26.225.113/ permanent;',
-  require => Package['nginx'],
-}
-
-file_line { 'Add custom header':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-  require => Package['nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
