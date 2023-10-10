ad9fa7aacb6061063770861dffb9da24e7803353
0xUgochukwu <0xUgochukwu@gmail.com>
2023-10-10 14:03:50 -0700

    pycodestlye

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,9 @@ def number_of_subscribers(subreddit):
     import requests
 
     res = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+                       .format(subreddit),
+                       headers={"User-Agent": "My-User-Agent"},
+                       allow_redirects=False)
     if res.status_code >= 300:
         return 0
 
