0ecd0372cc072a1a2ba25a0c4875992210a0f153
0xUgochukwu <0xUgochukwu@gmail.com>
2023-10-11 14:10:07 -0700

    fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # Webstack debugging #3
 exec { 'Fix Wordpress':
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php"
 }
